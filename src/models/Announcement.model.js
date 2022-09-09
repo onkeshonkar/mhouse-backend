48affc7d23d4e105402cd82ae26b8a875242b81b
onkeshonkar <onkeshkumaronkar315@gmail.com>
2022-09-09 17:49:44 +0530

    model: Announcement

---
+++ src/models/Announcement.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Types;
+
+const toJSON = require("../utils/toJSON");
+
+const announceSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    mediaUrl: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    branch: {
+      type: ObjectId,
+      ref: "Branch",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+toJSON(announceSchema, false);
+
+const Announcement = mongoose.model("Announcement", announceSchema);
+
+module.exports = Announcement;
