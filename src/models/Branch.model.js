388a7eb9736e41175956c8f7cfe5a7e013b38254
onkeshonkar <onkeshkumaronkar315@gmail.com>
2022-09-09 17:51:15 +0530

    model : Branch and Restaurent

---
+++ src/models/Branch.model.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Types;
+
+const toJSON = require("../utils/toJSON");
+
+const branchSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    manager: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isMainBranch: Boolean,
+    address: String,
+    bankName: String,
+    bankAccount: String,
+    currency: String,
+    timeFormat: {
+      type: String,
+      enum: ["12 hr", "24 hr"],
+      default: "12 hr",
+    },
+    timeZone: String,
+    payrollType: {
+      type: String,
+      enum: ["Weekly", "Fortnightly", "Monthly"],
+      default: "Weekly",
+    },
+    cashRegister: {
+      type: Number,
+      default: 0.0,
+    },
+    safeDeposit: {
+      type: Number,
+      default: 0.0,
+    },
+    closingAmount: {
+      type: Number,
+      default: 0.0,
+    },
+    departments: [String],
+    jobTitles: [String],
+    roles: [String],
+    notification: {
+      type: Object,
+      default: {
+        "Tasks Assigned To You": ["email", "sms"],
+        "New Transfer In Finance": ["email", "sms"],
+        "Forecast Update": [],
+        "Targets Update": [],
+        "Update In Suppliers": [],
+        "Items Received": ["email"],
+        "Update Availability Of Employees": ["email"],
+      },
+    },
+    restaurent: {
+      type: ObjectId,
+      ref: "Restaurent",
+      required: true,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+branchSchema.index({ name: 1, restaurent: 1 }, { unique: true });
+
+toJSON(branchSchema);
+
+const Branch = mongoose.model("Branch", branchSchema);
+
+module.exports = Branch;
---
+++ src/models/Restaurent.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const toJSON = require("../utils/toJSON");
+
+const { ObjectId } = mongoose.Types;
+
+const restaurentSchema = mongoose.Schema(
+  {
+    venueName: {
+      //  venue name will be the branch name for main branch
+      type: String,
+      required: true,
+    },
+    businessName: String, //  eg. abc.pvt ltd.
+    address: String, // main branch address
+    owner: {
+      type: ObjectId,
+      ref: "User",
+    },
+    payrollId: {
+      type: Number,
+      default: 0,
+    },
+    employeeId: {
+      type: Number,
+      default: 0,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+toJSON(restaurentSchema);
+
+const Restaurent = mongoose.model("Restaurent", restaurentSchema);
+
+module.exports = Restaurent;
