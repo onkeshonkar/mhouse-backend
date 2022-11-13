c3c428d153dea01817bf6f7ee545c155bc272a5b
onkeshonkar <onkeshkumaronkar315@gmail.com>
2022-11-13 17:41:08 +0530

    feat: get SupplierBy id route.

---
+++ src/components/supplier/controllers/getSupplierById.controller.js
@@ -0,0 +1,33 @@
+const httpStatus = require("http-status");
+const Joi = require("joi");
+
+require("express-async-errors");
+
+const ApiError = require("../../../utils/ApiError");
+const validateSchema = require("../../../utils/validateSchema");
+const customValidators = require("../../../utils/customValidator");
+const canAccess = require("../../../utils/canAccess");
+
+const Supplier = require("../../../models/Suppliers.model");
+
+module.exports = async (req, res, next) => {
+  const schema = {
+    params: Joi.object({
+      branchId: Joi.string().custom(customValidators.objectId).required(),
+      supplierId: Joi.string().custom(customValidators.objectId).required(),
+    }),
+  };
+
+  validateSchema(req, schema);
+  if (!canAccess(req.user, "SUPPLIER", "view")) {
+    throw new ApiError(
+      httpStatus.FORBIDDEN,
+      "You are not allowed to access this resource"
+    );
+  }
+  const { branchId, supplierId } = req.params;
+
+  const supplier = await Supplier.findOne({ supplier: supplierId });
+
+  res.send({ supplier });
+};
--- src/components/supplier/supplier.routes.js
+++ src/components/supplier/supplier.routes.js
@@ -4,8 +4,10 @@ const router = express.Router({ mergeParams: true });
 
 const addSupplierController = require("./controllers/addSupplier.controller");
 const getSuppliersController = require("./controllers/getSuppliers.controller");
+const getSupplierByIdController = require("./controllers/getSupplierById.controller");
 
 router.post("/", addSupplierController);
 router.get("/", getSuppliersController);
+router.get("/:supplierId", getSupplierByIdController);
 
 module.exports = router;
